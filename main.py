23bb7789194464c965bba5d1ff4a5b74c476dd73
danielle <danielle@trellis.ag>
2020-04-07 12:51:37 +0300

    split class

--- main.py
+++ main.py
@@ -2,33 +2,7 @@ from src.face_detection import FaceDetection
 
 
 def main():
-    FaceDetection(img_path="src/statics/fake_ai_faces.0.png").process_detect_faces()
-
-
-
-# def main():
-#     face_cascade = cv2.CascadeClassifier("src/statics/haarcascade_frontalface_default.xml")
-#     face_img = cv2.imread("src/statics/news.jpg")
-#     gray_img = cv2.cvtColor(face_img, cv2.COLOR_BGR2GRAY)
-#
-#     faces = face_cascade.detectMultiScale(gray_img,
-#                                           scaleFactor=1.3,
-#                                           minNeighbors=5,
-#                                           minSize=(30, 30),
-#                                           flags=cv2.CASCADE_SCALE_IMAGE)
-#     for x, y, w, h in faces:
-#         face_img = cv2.rectangle(face_img, (x, y),
-#                                  (x + w, y + h),
-#                                  (0, 255, 0),
-#                                  2)
-#
-#     resized = cv2.resize(face_img,
-#                          (int(face_img.shape[1] / 3),
-#                           int(face_img.shape[0] / 3)))
-#
-#     cv2.imshow("Gray", resized)
-#     cv2.waitKey(0)
-#     cv2.destroyAllWindows()
+    FaceDetection().process_detect_faces(img_path="src/statics/fake_ai_faces.0.png")
 
 
 if __name__ == '__main__':
--- src/face_detection.py
+++ src/face_detection.py
@@ -3,26 +3,23 @@ from src.constants import FaceDetectionConstants
 
 
 class FaceDetection:
-    def __init__(self, img_path):
+    def __init__(self):
         self.face_cascade = cv2.CascadeClassifier(FaceDetectionConstants.CASCADE_CLASSIFIER_SRC)
-        self.origin_face_img = cv2.imread(img_path)
         self.gray_img = None
+        self.origin_img = None
+        self.resized_faces_img = None
         self.rectangle_faces_img = None
-        self.resized_rectangle_faces_img = None
 
-    def process_detect_faces(self):
-        self.convert_img_to_gray()
-        self.extract_faces_from_image()
-        self.resize_img()
-        self.show_img()
+    def process_detect_faces(self, img_path):
+        self.origin_img = ImgProcess.read_img(img_path)
+        self.gray_img = ImgProcess.convert_img_to_gray(self.origin_img)
+        self.extract_faces_from_image(self.gray_img)
+        self.resized_faces_img = ImgProcess.resize_img(self.rectangle_faces_img)
+        ImgProcess.show_img(self.resized_faces_img)
 
-    def convert_img_to_gray(self):
-        gray_img = cv2.cvtColor(self.origin_face_img, cv2.COLOR_BGR2GRAY)
-        self.gray_img = gray_img
-
-    def extract_faces_from_image(self):
+    def extract_faces_from_image(self, gray_img):
         faces = self.face_cascade.detectMultiScale(
-            self.gray_img,
+            gray_img,
             scaleFactor=FaceDetectionConstants.SCALE_FACTOR,
             minNeighbors=FaceDetectionConstants.MIN_NEIGHBORS,
             minSize=FaceDetectionConstants.MIN_SIZE,
@@ -30,18 +27,31 @@ class FaceDetection:
         )
         print("Found {0} faces!".format(len(faces)))
         for x, y, w, h in faces:
-            self.rectangle_faces_img = cv2.rectangle(self.gray_img, (x, y),
+            self.rectangle_faces_img = cv2.rectangle(self.origin_img, (x, y),
                                                      (x + w, y + h),
                                                      color=FaceDetectionConstants.RECTANGLE_COLOR,
                                                      thickness=FaceDetectionConstants.RECTANGLE_THICKNESS)
 
-    def resize_img(self):
-        img_shape = self.rectangle_faces_img.shape
-        self.resized_rectangle_faces_img = cv2.resize(src=self.rectangle_faces_img,
-                                                      dsize=(int(img_shape[1] / 3),
-                                                             int(img_shape[0] / 3)))
 
-    def show_img(self):
-        cv2.imshow(FaceDetectionConstants.WINDOW_NAME, self.resized_rectangle_faces_img)
+class ImgProcess:
+
+    @staticmethod
+    def read_img(img_path):
+        return cv2.imread(img_path)
+
+    @staticmethod
+    def convert_img_to_gray(img):
+        gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        return gray_img
+
+    @staticmethod
+    def resize_img(img):
+        return cv2.resize(src=img,
+                          dsize=(int(img.shape[1] / 3),
+                                 int(img.shape[0] / 3)))
+
+    @staticmethod
+    def show_img(img):
+        cv2.imshow(FaceDetectionConstants.WINDOW_NAME, img)
         cv2.waitKey(0)
         cv2.destroyAllWindows()
